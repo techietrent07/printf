7100fa4cb856472ba466c264f00b1f950ca7110f
peetah001 <okpelepeter001@gmail.com>
2023-08-23 11:56:45 -0700

    test alternative

--- _printf.c
+++ _printf.c
@@ -1,58 +1,52 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
-*/
+ * _printf - function to replicate printf
+ * @format: format to be printed.
+ *
+ * Return: 0 if successful.
+ */
+
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return (-1);
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-}
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list,  buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
-/**
-* print_buffer - Prints the contents of the buffer if it exist
-* @buffer: Array of chars
-* @buff_ind: Index at which to add next char, represents the length.
-*/
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
-}
+	va_list arg;
+	int i, j, no_c = 0;
 
+	print_d data[] = {
+		{"c", pc}, {"s", ps}, {"%", pp}, {"d", dc}, {"u", dcu}, {"i", dc},
+		{"b", bc}, {"S", ps}
+	}; /** add print dig call and all after **/
+	va_start(arg, format);
+	if (!format)
+		return (-1);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			no_c++;
+		}
+		else
+		{
+			if (!format[i + 1] || format[i + 1] == ' ')
+				return (-1);
+			for (j = 0; j < 8; j++) /** changed j < 3 to j < 6 **/
+			{
+				if (format[i + 1] == *(data[j].c))
+					break;
+			}
+			if (j < 8) /** changed j < 3 to j < 6 **/
+			{
+				no_c = no_c + data[j].f_pr(arg);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				no_c++;
+			}
+		}
+	}
+	va_end(arg);
+	return (no_c);
+}
--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,10 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 0 on success.
  */
 
 int _putchar(char c)
